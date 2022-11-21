a0dee5bfd7e033de70bd57744374acf60518c3a6
reneenh <reneehamlinteaches@gmail.com>
2022-11-20 22:43:44 -0800

    initial setup of html and javascript files, with comments for guidance

---
+++ assets/script.js
@@ -0,0 +1,19 @@
+// elements--need to tie to html so it will generate correctly.
+
+// questions for the quiz
+
+// variables?
+
+// functions to render questions - call questions
+
+// function to start quiz
+
+// function to show progress
+
+// timer/countdown function, I think
+
+// display the user's answers
+
+// what if the answer is right?
+
+// what if the answer is wrong?
