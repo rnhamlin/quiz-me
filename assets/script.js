a3f7f7d8487831f65eb4eb7dc327bc45761cc307
reneenh <reneehamlinteaches@gmail.com>
2022-11-26 08:51:46 -0800

    fixed some js code, incl. the countdown function, tick function, multiple mismatched variables, and several punctuation errors

--- assets/script.js
+++ assets/script.js
@@ -1,79 +1,101 @@
-const { mapOptionFieldNames } = require("sequelize/types/utils");
-
 // elements--need to tie to html so it will generate correctly.
 const startnow = document.getElementById("startnow");
 const timer = document.getElementById("timer");
-var question = document.getElementById("question");
 const option1 = document.getElementById("One");
 const option2 = document.getElementById("Two");
 const option3 = document.getElementById("Three");
-const progress = document.getElementById("progress");
+let currentTime=document.querySelector("#currentTime");
+let questionSection = document.querySelector("#questionsDiv")
+let currentQuestion = document.getElementById("currentquestion")
+
 
 // questions for the quiz
 
 let question = [
   {
     question: "How do you add a comment in Javascript?",
-    option1: "",
-    option2: "",
-    option3: "",
-    correct: "1",
+    option1: "// comment",
+    option2: "~~ comment",
+    option3: "<!--comment-->",
+    correct: "// comment",
   },
   {
     question: "Is Javascript the same as Java?",
-    option1: "",
-    option2: "",
-    option3: "",
-    correct: "3",
+    option1: "Yes",
+    option2: "What's coffee got to do with it?",
+    option3: "No",
+    correct: "No",
   },
   {
     question:
       "How do you make something happen when a user clicks their mouse?",
-    option1: "",
-    option2: "",
-    option3: "",
+    option1: "Add an event listener",
+    option2: "Draw an arrow",
+    option3: "Write '===='",
     correct: "1",
   },
   {
     question:
       "Where do you add the link to your Javascript file in your HTML file?",
-    option1: "",
-    option2: "",
-    option3: "",
+    option1: "In the footer",
+    option2: "In the header or the body",
+    option3: "In the header",
     correct: "2",
-  },
+  }
 ];
 
-// variables?
+// variables
+let score=0;
+let questionIndex=0;
 
 // functions to render questions - call questions
 
 function askQuestion(){
-  let q = questions[currentques];
-  question.innerHTML = "<p>" = q.question + "</p>";
+  let q = question[questionIndex];
+  currentQuestion.innerHTML = "<p>" = q.question + "</p>";
   option1.innerHTML = q.option1;
   option2.innerHTML = q.option2;
   option3.innerHTML = q.option3;
 }
 
-start.addEventListener("click",startQuiz)
+
 
 // function to start quiz
 
 function startQuiz() {
   startQuiz.style.display = "none";
-  renderQuestion();
-  renderProgress();
-  renderTimer();
-  TIMER = setInterval(renderTimer, 2000);
+  askQuestion();
+  countdown();
 }
 
 // function to show progress
+function tick() {
+        secondsLeft--;
+      currentTime.textContent = "Time: " + secondsLeft;
+
+      if (secondsLeft <= 0) {
+        clearInterval(holdInterval);
+        allDone();
+          currentTime.textContent = "Time's Up! Game over!";
+      }
+}
+
 
 // timer/countdown function, I think
+function countdown() {
+  var clock = setInterval(tick, 1000);
+};
+
 
 // display the user's answers
 
 // what if the answer is right?
 
 // what if the answer is wrong?
+
+
+// event listeners go at the bottom
+//need second event listener for submit answer button, which connects to countdown/tick
+
+startnow.addEventListener("click",startQuiz)
+
